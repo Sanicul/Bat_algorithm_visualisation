56e6b568d74e094f3f1ebf8a134b4cc92e4a5181
Sanicul <tolasavinov@gmail.com>
2024-02-15 09:35:41 +0300

    draw_bats добавлена

--- visualisation.py
+++ visualisation.py
@@ -2,8 +2,15 @@ from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QGraphicsView
 from PyQt5.QtGui import QBrush, QPen, QColor
 from PyQt5.QtCore import Qt
 from window import Ui_MainWindow
+from BatAlgorithm import BatAlgorithm
+
 
 class MainWindow(QMainWindow):
+    pointsize = 10
+    black_pen = QPen(QColor.fromRgb(0, 0, 0))
+    bat_color = QBrush(QColor.fromRgb(196, 108, 0))
+    algorithm =
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
@@ -11,5 +18,6 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
 
     def draw_bats(self, x):
-
-
+        for elem in x:
+            self.scene.addEllipse(elem[0] - self.pointsize / 2, elem[1] - self.pointsize / 2, self.pointsize,
+                                  self.pointsize, self.black_pen, self.bat_color)
