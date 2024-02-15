d944f19bbf89b49e8d4f5320c7c4b59eda84c4b4
Sanicul <tolasavinov@gmail.com>
2024-02-15 12:33:29 +0300

    one iteration and normalise and draw

--- visualisation.py
+++ visualisation.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QGraphicsView
 from PyQt5.QtGui import QBrush, QPen, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRect
 from window import Ui_MainWindow
 import numpy as np
 from BatAlgorithm import BatAlgorithm
@@ -16,21 +16,35 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.scene = QGraphicsScene()
+        self.ui.graphicsView.setScene(self.scene)
         self.alg = alg
-        self.ui.oneIteration.clicked.connect(self.draw_bats)
+        self.ui.oneIteration.clicked.connect(self.do_iteration)
         self.ui.initialize.clicked.connect(self.set_initial_distribution)
 
-    def draw_bats(self, x):
-        for elem in x:
+    def draw_bats(self):
+        self.scene.clear()
+
+        a = self.alg.x.copy()
+        a = (a + 10) * 39
+
+        for elem in a:
             self.scene.addEllipse(int(elem[0]) - self.pointsize / 2, int(elem[1]) - self.pointsize / 2,
                                   self.pointsize, self.pointsize,
                                   self.black_pen, self.bat_color)
 
+    def do_iteration(self):
+        for i in range(10):
+            self.alg.oneIteration()
+        self.draw_bats()
+
+    def normalize_and_draw_bats(self):
+        a = self.alg.x.copy()
+        a = (a + 10) * 39
+        self.draw_bats(a)
+
     def set_initial_distribution(self):
         self.alg.proses_init()
-        a = self.alg.x.copy()
-        a = (a + 10) * 10
-        self.draw_bats(a)
+        self.draw_bats()
 
 
 
